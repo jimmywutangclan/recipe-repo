f78a40516baafbb323bc32fb2c835c6a85d76480
jimmywutangclan <jw29731@gmail.com>
2021-11-29 18:11:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>Recipes</h2>
+        <h2>Make a recipe</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
