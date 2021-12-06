ea7204f5668a32ff820fea2ccf7055e5594e5f55
jimmywutangclan <jw29731@gmail.com>
2021-12-06 13:30:08 -0500

    Added ability to clear all recipes

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function MainMenu(props) {
+    const [ clearing, setClearing ] = useState(false);
+
+    const clearRecipes = (event) => {
+        event.preventDefault();
+        setClearing(false);
+        props.clearLocalStorage();
+    }
+
+    const toggleClear = (event, val) => {
+        event.preventDefault();
+        setClearing(val);
+    }
+
     return (
         <div>
             <a href="/submit">Submit new recipe</a>
+            { clearing ? <label>Are you sure you want to clear?<button onClick={clearRecipes}>Yes</button><button onClick={(event) => toggleClear(event, false)}>No</button></label> : 
+            <button onClick={(event) => toggleClear(event, true)}>Clear recipes</button>}
             {props.recipePreviews}
         </div>
     )
--- src/Components/RecipesApp.js
+++ src/Components/RecipesApp.js
@@ -33,6 +33,12 @@ export default class RecipesApp extends Component {
         localStorage.setItem('recipes', JSON.stringify(recipes));
     };
 
+    // clear local storage
+    clearLocalStorage = () => {
+        localStorage.clear();
+        this.setState({ recipes: [] });
+    };
+
     render() {
         const recipeList = this.state.recipes;
         const recipeHtml = recipeList.map((recipe, index) => <h2><a href={"/recipes/" + index}>{recipe.name}</a></h2>)
@@ -42,7 +48,7 @@ export default class RecipesApp extends Component {
                 <div>
                     <h1><a href="/">Recipes</a></h1>
                     <Routes>
-                        <Route path="/" element={<MainMenu recipePreviews={recipeHtml} />} />
+                        <Route path="/" element={<MainMenu recipePreviews={recipeHtml} clearLocalStorage={this.clearLocalStorage}/>} />
                         <Route path="/submit" element={<AddRecipe submitRecipe={this.submitRecipe} futureIndex={this.state.recipes.length}/>} />
                         <Route path="/recipes/:id" element={<Recipe recipes={this.state.recipes} replaceRecipeList={this.replaceRecipeList}/>} />
                         <Route path="/edit/:id" element={<EditRecipe recipes={this.state.recipes} replaceRecipe={this.replaceRecipe}/>} />
